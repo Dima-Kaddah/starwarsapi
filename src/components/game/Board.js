96c8b6f3ae5620f017167525ff119978f2f0af97
Dima <dimakaddah@gmail.com>
2020-11-09 22:21:29 +0100

    make it beter responsive

--- src/components/game/Board.js
+++ src/components/game/Board.js
@@ -11,7 +11,6 @@ const Board = ({
         {youWin ? <Redirect to='/win' /> : (
           cards.map(card => <Card key={card.id} id={card.id} type={card.type} width={dimension / 4.5} height={dimension / 4.5} flipped={flipped.includes(card.id)} handleClick={handleClick} disabled={disabled || solved.includes(card.id)} solved={solved.includes(card.id)} />)
         )}
-
       </div >
     </div >
   );
--- src/components/game/WinPage.js
+++ src/components/game/WinPage.js
@@ -5,10 +5,10 @@ import winGIf from '../../images/winGif.gif';
 
 const WinPage = () => {
   return (
-    <div>
-      <img style={{ borderRadius: '20px' }} src={winGIf} />
-      <h1 style={{ padding: '20px', color: 'yellow' }}>you win</h1>
-      <Link to='/game'><button style={{ margin: '3px' }}>Play Again</button></Link>
+    <div className='flexWin'>
+      <img className='winImge flexWin' src={winGIf} />
+      <h1 className='flexWin' style={{ padding: '20px', color: 'yellow' }}>you win</h1>
+      <Link to='/game' className='restbutton'><button className='flexWin restbutton'>Play Again</button></Link>
     </div >
   );
 };
